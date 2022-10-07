d4007c65734dace8dcb226d86275ef5ba5f2b0ad
Happy <utkarsh21xxx004@akgec.ac.in>
2022-10-07 22:44:55 +0530

    ok

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+        child: Column(
+      children: [Image.asset("assets/images/Happy.jpg")],
+    ));
+  }
+}
+
+
+
+
+
+
+
+
+
+/* child: Center(
+        child: Text(
+          "log in my page",------->>>>>>this styling in font
+          style: TextStyle(
+              fontSize: 40,
+              color: Colors.deepPurple,
+              fontWeight: FontWeight.bold),
+        ),
+      ),
+    );*/
