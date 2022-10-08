2fba5a40c5a472e95b773ac9d4d6877350b48b63
Happy <utkarsh21xxx004@akgec.ac.in>
2022-10-08 23:48:15 +0530

    day4 completed

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_app/home_page.dart';
 import 'package:flutter_app/pages/login_page.dart';
 
+
 void main() {
   runApp(MyApp());
 }
@@ -13,8 +14,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         themeMode: ThemeMode.dark,
-        theme: ThemeData(primarySwatch: Colors.grey),
+        theme: ThemeData(primarySwatch: Colors.grey,
+     //   fontFamily: GoogleFonts.lato().fontFamily,---->>>not un
+        ),
         darkTheme: ThemeData(primarySwatch: Colors.green),
+      
        // initialRoute: "/home",--->>open the page of first create
         routes: {
           "/": (context) => LoginPage(),
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -4,7 +4,75 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
+      color: Colors.white,
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.asset(
+              "assets/images/undraw_join_re_w1lh.png",
+              height: 400,
+              width: 500,
+              fit: BoxFit.cover,
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Text("Welcome",
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                )),
+            SizedBox(
+              height: 20.0,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 45),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "enter your userId",
+                      labelText: "Usernmae",
+                    ),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: "enter your password",
+                      labelText: "Password",
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      print("wooyah");
+                    },
+                    child: Text("login"),
+                    style: TextButton.styleFrom(),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+ /*child: Center(
         child: Text(
           "log in my page", //------->>>>>>this styling in font
           style: TextStyle(
@@ -13,7 +81,4 @@ class LoginPage extends StatelessWidget {
               fontWeight: FontWeight.bold),
       //    textScaleFactor: 2.0,--->>increase the size of font
         ),
-      ),
-    );
-  }
-}
+      ),*/
